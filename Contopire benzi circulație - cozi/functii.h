2c2a3a0110efd5cea59c2c7a5b6c0acad4c2d367
stefanv129 <159702007+stefanv129@users.noreply.github.com>
2024-02-11 15:51:17 +0200

    Update functii.h

--- Contopire benzi circulație - cozi/functii.h
+++ Contopire benzi circulație - cozi/functii.h
@@ -5,27 +5,18 @@
 #include<stdlib.h>
 #include<string.h>
 
-struct NOD{
+// Structure definition for a node
+struct NOD {
     char nr[8];
     struct NOD *next;
 };
 
-
+// Function declarations
 struct NOD * creere_nod(char nr[8]);
-struct NOD * add_nod(struct NOD * head,char nr[8]);
+struct NOD * add_nod(struct NOD * head, char nr[8]);
 void show(struct NOD * head);
-struct NOD * funnel(struct NOD * head1,struct NOD * head2,struct NOD * head3);
+struct NOD * funnel(struct NOD * head1, struct NOD * head2, struct NOD * head3);
 char * peek(struct NOD * head);
 struct NOD * del_nod(struct NOD * head);
 
-
-
-
-
-
-
-
-
-
-
-#endif
+#endif // FUNCTII_H
