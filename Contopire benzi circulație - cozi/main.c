bb837bd1fe4cd149e30421fdcf3bb7efef38ec36
stefanv129 <159702007+stefanv129@users.noreply.github.com>
2024-02-11 15:51:59 +0200

    Update main.c

--- Contopire benzi circulație - cozi/main.c
+++ Contopire benzi circulație - cozi/main.c
@@ -8,29 +8,35 @@
 #include "functii.h"
 
 
-int main(){
-    int m,n;
+#include "functii.h"
+
+int main() {
+    int m, n;
     char temp[8];
-    struct NOD * band1=NULL;
-    struct NOD * band2=NULL;
-    struct NOD * band3=NULL;
+    struct NOD *band1 = NULL;
+    struct NOD *band2 = NULL;
+    struct NOD *band3 = NULL;
     
-    scanf("%d",&m);
-    scanf("%d",&n);
-    
-    for(int i=0;i<m;i++){
+    // Read the number of cars in the first and second lanes
+    scanf("%d", &m);
+    scanf("%d", &n);
+
+    // Populate the first lane with cars
+    for (int i = 0; i < m; i++) {
         scanf("%s", temp);
-        band1=add_nod(band1,temp);
+        band1 = add_nod(band1, temp);
     }
-    
-    for(int j=0;j<n;j++){
+
+    // Populate the second lane with cars
+    for (int j = 0; j < n; j++) {
         scanf("%s", temp);
-        band2=add_nod(band2,temp);
+        band2 = add_nod(band2, temp);
     }
-    //band1=del_nod(band1);
-    //show(band1);
-    //show(band2);
-    band3=funnel(band1,band2,band3);
+
+    // Combine and display the interleaved order of cars after merging lanes
+    band3 = funnel(band1, band2, band3);
     show(band3);
-    
+
+    return 0;
 }
+
